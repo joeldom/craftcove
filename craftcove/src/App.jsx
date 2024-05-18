e00eaf3956411c9aa76330bb326f91af6c5f114d
Joel <dombekj890@gmail.com>
2024-05-18 04:47:50 -0400

    fixes for dark/light button context and pos

--- craftcove/src/App.jsx
+++ craftcove/src/App.jsx
@@ -21,9 +21,10 @@ const App = () => {
 
     return (
         <div className={isDarkTheme ? 'container dark-mode' : 'container light-mode'}>
-            <h1>Welcome to David Lambel's Portfolio</h1>
+            <h1>David Lambl</h1>
+            <h3>Welcome to portfolio of Pittsburgh based developer David Lambl. Here is where I showcase projects and pattern as well as continuously iterate on my workflow.</h3>
             <button className="toggle-button" onClick={toggleTheme}>
-                Toggle to {isDarkTheme ? 'Light' : 'Dark'} Mode
+                {isDarkTheme ? 'Dark' : 'Light'} Mode
             </button>
         </div>
     );
