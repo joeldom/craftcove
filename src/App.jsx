3ffd7f81a25730a3438e54c281cf6e9b80f6c64a
David Lambl <david.lambl@gmail.com>
2024-05-30 23:41:33 -0400

    Refactor code and import image asset
    This commit involves refactoring some parts of the App module and importing an image asset. From now on, the 'React' import is replaced with only 'useState' since 'React' is not in use. An image asset, 'bird.png', is also imported at the top of the file and correctly referenced in the image source attribute.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
+import bird from './assets/bird.png';
 
 const darkTheme = {
     background: '#000',
@@ -20,14 +21,14 @@ const App = () => {
     };
 
     return (
-        <div className={isDarkTheme ? 'container dark-mode' : 'container light-mode'}>
-            <h1>David Lambl</h1>
-            <h3>Welcome to portfolio of Pittsburgh based developer David Lambl. Here is where I showcase projects and pattern as well as continuously iterate on my workflow.</h3>
-            <button className="toggle-button" onClick={toggleTheme}>
-                {isDarkTheme ? 'Dark' : 'Light'} Mode
-            </button>
-            <img className="bird" src='assets/bird.png'/>
-        </div>
+      <div className={isDarkTheme ? 'container dark-mode' : 'container light-mode'}>
+        <h1>David Lambl</h1>
+        <h3>Welcome to portfolio of Pittsburgh based developer David Lambl. Here is where I showcase projects and pattern as well as continuously iterate on my workflow.</h3>
+        <button className="toggle-button" onClick={toggleTheme}>
+          {isDarkTheme ? 'Dark' : 'Light'} Mode
+        </button>
+        <img className="bird" src={bird}  alt={'bird'}/>
+      </div>
     );
 };
 
